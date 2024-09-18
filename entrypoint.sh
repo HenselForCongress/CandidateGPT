62913e515445d6cde9ba6508763cbb99a3bba2df
Bentley Hensel <bentleyhensel@gmail.com>
2024-09-18 18:04:14 -0400

    Added Langfuse Database
    Signed-off-by: Bentley Hensel <bentleyhensel@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,17 @@ done
 echo "Running database migrations..."
 poetry run alembic upgrade head
 
+# Create the langfuse database if it doesn't already exist
+echo "Checking if 'langfuse' database exists..."
+DB_EXISTS=$(psql -h db_postgres -U "$POSTGRES_USER" -tAc "SELECT 1 FROM pg_database WHERE datname='langfuse'")
+
+if [ "$DB_EXISTS" != "1" ]; then
+  echo "Creating 'langfuse' database..."
+  psql -h db_postgres -U "$POSTGRES_USER" -c "CREATE DATABASE langfuse;"
+else
+  echo "'langfuse' database already exists. Skipping creation."
+fi
+
 # Start the application
 echo "Starting the application..."
 exec "$@"
